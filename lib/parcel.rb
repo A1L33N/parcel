7bb983084a5bc229ffe00bd19d17526f547c578b
Aileen Cacayorin & Jing Wait <student@epicodus.com>
2015-08-10 14:13:55 -0700

    It will factor parcel size into total shipping cost>

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -5,6 +5,7 @@ class Parcel
     @height = height
     @weight = weight
     @volume = volume
+    @size = size
   end
 
 
@@ -12,6 +13,38 @@ class Parcel
   define_method(:volume) do
     volume = (@length.*(@width)).*(@height)
     volume
+
+  end
+
+  define_method(:size) do
+    if @volume.<= 125
+      size ="S"
+
+    elsif @volume.<= 1728
+      size ="M"
+
+    else
+      size ="L"
+
+    end
+    size
+  end
+
+  define_method(:cost_to_ship) do
+    cost = 0
+    if @size.=="S"
+      size_cost = 5
+    elsif @size.=="M"
+        size_cost = 10
+    else
+      size_cost = 20
+    end
+
+    #weight_cost = @weight.*(.1)
+
+    cost = cost.+(size_cost)
+
+
 
   end
 
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -10,4 +10,22 @@ describe(Parcel) do
     end
   end
 
+  describe(:size) do
+    it("assigns a parcel size based on its volume") do
+      test_parcel = Parcel.new(5, 6, 6, 5)
+      expect(test_parcel.size()).to(eq("M"))
+    end
+  end
+
+  describe(:cost_to_ship) do
+    it("factors parcel size into cost") do
+      test_parcel = Parcel.new(6, 8, 7, 0)
+      expect(test_parcel.cost_to_ship()).to(eq(10))
+    end
+
+    #it("factors parcel weight into cost") do
+    #  test_parcel = Parcel.new(5, 5, 5, 10)
+    #  expect(test_parcel.cost_to_ship()).to(eq(15))
+    end
+  end
 end
